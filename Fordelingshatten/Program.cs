44320d61437e4a4939e631e0ff12275ac1ea645a
Jonas Steinmejer <jonas04@outlook.dk>
2023-09-13 09:03:25 +0200

    Finished Fordelingshatten

--- Fordelingshatten/Program.cs
+++ Fordelingshatten/Program.cs
@@ -8,9 +8,38 @@ namespace Fordelingshatten
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Hogwarts!");
 
+            // Instantiate the SortingHat
+            SortingHat sortingHat = new SortingHat();
+
+            // Call the AssignHouse method to determine the assigned house
+            Colleges assignedHouse = sortingHat.AssignHouse();
+
+            // Display the assigned house to the user
+            Console.WriteLine("The Sorting Hat has chosen your house:");
+
+            switch (assignedHouse)
+            {
+                case Colleges.Gryffindor:
+                    Console.WriteLine("You belong to Gryffindor - for the brave and strong!");
+                    break;
+                case Colleges.Hufflepuff:
+                    Console.WriteLine("You belong to Hufflepuff - for the loyal and kind-hearted!");
+                    break;
+                case Colleges.Ravenclaw:
+                    Console.WriteLine("You belong to Ravenclaw - for the curious and intelligent!");
+                    break;
+                case Colleges.Slytherin:
+                    Console.WriteLine("You belong to Slytherin - for the cunning and ambitious!");
+                    break;
+                default:
+                    Console.WriteLine("The Sorting Hat couldn't decide!");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Fordelingshatten/SortingHat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten
+{
+    enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slytherin };
+
+    class SortingHat
+    {
+        // Method to randomly assign a Hogwarts house
+        public Colleges AssignHouse()
+        {
+            // Generate a random number between 0 and 3
+            Random random = new Random();
+            int randomNumber = random.Next(0, 4);
+
+            // Convert the random number to a HogwartsHouse enum value
+            Colleges assignedHouse = (Colleges)randomNumber;
+
+            return assignedHouse;
+        }
+    }
+}
